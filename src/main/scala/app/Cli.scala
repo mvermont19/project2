ecc454bd73bf343b21e178ae7d7001a2554cffad
Kyle Kinsey <kinseywk@gmail.com>
2021-11-10 18:25:40 -0500

    Remove starter template artifacts

--- src/main/scala/app/Cli.scala
+++ src/main/scala/app/Cli.scala
@@ -2,7 +2,7 @@ package app
 
 import scala.io.StdIn.readLine
 
-object Cli extends Greeting with App {
+object Cli extends App {
   val MAIN_MENU = "Main Menu\n 1: TODO\n 2: Quit"
 
   var state = 0
@@ -24,8 +24,4 @@ object Cli extends Greeting with App {
 
     print("\u001b[2J")
   } while(state != -1)
-}
-
-trait Greeting {
-  lazy val greeting: String = "hello"
 }
