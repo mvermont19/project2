e9ca5334779b3dd069b176c7f93c2eda2817cbd9
Matthew Bowman <mbowman27x@gmail.com>
2021-11-11 12:49:15 -0500

    API to HDFS Complete

--- src/main/scala/app/Twitter.scala
+++
@@ -1,6 +0,0 @@
-package app
-
-object Twitter {
-    //TODO
-    case class Tweet(username: String, text: String)
-}
--- src/main/scala/app/package.scala
+++
@@ -1,5 +0,0 @@
-package app
-
-package object app {
-    val MAIN_MENU = "Main Menu\n 1: TODO\n 2: Quit"
-}
