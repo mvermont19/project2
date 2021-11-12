a315d5f1b581931c32ead016e6fcaf8ee299ba8a
Kyle Kinsey <kinseywk@gmail.com>
2021-11-12 10:07:04 -0500

    Add API scraping menu

--- src/main/scala/app/Cli.scala
+++ src/main/scala/app/Cli.scala
@@ -1,6 +1,7 @@
 package app
 
 import app._
+import misc._
 import scala.io.StdIn.readLine
 
 object Cli extends App {
@@ -13,9 +14,46 @@ object Cli extends App {
     print(s"${menu + "\n\nOption: "}")
 
     state match {
+      //Main menu = 0
       case 0 => {
         readLine() match {
+          //Transition to Scrape menu
+          case "1" => {
+            state = 1
+            menu = SCRAPE_MENU
+          }
+          //Exit app
           case "2" => state = -1
+          //Do nothing (bad input)
+          case _ => {}
+        }
+      }
+
+      //API scraping menu = 1
+      case 1 => {
+        readLine() match {
+          //Stock
+          case "1" => {
+            print("Company: ")
+            val name = readLine()
+            print("Ticker symbol: ")
+            val symbol = readLine()
+            scrape(Stock(name, symbol))
+          }
+          //Cryptocurrency
+          case "2" => {
+            print("Cryptocurrency: ")
+            val name = readLine()
+            print("Ticker symbol: ")
+            val symbol = readLine()
+            scrape(Cryptocurrency(name, symbol))
+          }
+          //Previous menu
+          case "3" => {
+            state = 0
+            menu = MAIN_MENU
+          }
+          //Do nothing (bad input)
           case _ => {}
         }
       }
--- src/main/scala/app/package.scala
+++ src/main/scala/app/package.scala
@@ -1,5 +1,14 @@
 package app
 
+import misc._
+
 object `package` {
-    val MAIN_MENU = "Main Menu\n 1: TODO\n 2: Quit"
+    val MAIN_MENU = "Main Menu\n 1: Scrape securities data\n 2: Quit"
+    val SCRAPE_MENU = "Which type of security?\n 1: Stock\n 2: Cryptocurrency"
+    val DATA_DIRECTORY = "data/"
+    val SECURITIES_DB_PATH = s"${DATA_DIRECTORY}db.json"
+
+    def scrape(security: Security) {
+
+    }
 }
