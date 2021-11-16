49ec6587f4b09162966d41fca62cbcbc9fe7f6bd
Kyle Kinsey <kinseywk@gmail.com>
2021-11-16 00:44:03 -0500

    Add .env to deployment regime

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
+sshpass -p maria_dev scp -P 2222 run.sh maria_dev@sandbox-hdp.localhost:/home/maria_dev
 sshpass -p maria_dev scp -P 2222 target/scala-2.11/project2.jar maria_dev@sandbox-hdp.localhost:/home/maria_dev
