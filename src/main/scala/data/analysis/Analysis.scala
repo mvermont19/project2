1438ebe7efe33feabbf607be1fde381977fe834c
Kyle Kinsey <kinseywk@gmail.com>
2021-11-16 18:26:35 -0500

    Fix .gitignore, was ignoring **/data/, should ignore /data/

---
+++ src/main/scala/data/analysis/Analysis.scala
@@ -0,0 +1,62 @@
+package data.analysis
+
+import data.analysis._
+import misc._
+import com.github.nscala_time.time.Imports._
+
+object Analysis {
+    /** Find a highest price a given security traded at
+    * @param security A stock symbol or cryptocurrency
+    * @return The highest price the security ever traded at and the date that occurred
+    */
+    def historicalHighPrice(security: Security): (Float, DateTime) = {
+        //TODO
+        return (0.0f, DateTime.now())
+    }
+
+    /** Find a lowest price a given currency traded at
+     * @param security A stock symbol or cryptocurrency
+     * @return The lowest price the security ever traded at and the date that occurred
+    */
+    def historicalLowPrice(security: Security): (Float, DateTime) = {
+        //TODO
+        return (0.0f, DateTime.now())
+    }
+    //}
+
+    /** Find a security's most active trading day
+     * @param security A stock symbol or cryptocurrency
+     * @return The most units the security traded in any day and the date that occurred
+    */
+    def historicalHighVolume(security: Security): (Float, DateTime) = {
+        //TODO
+        return (0.0f, DateTime.now())
+    }
+
+    /** Find a security's least active trading day
+     * @param security A stock symbol or cryptocurrency
+     * @return The least units the security traded in any day and the date that occurred
+    */
+    def historicalLowVolume(security: Security): (Float, DateTime) = {
+        //TODO
+        return (0.0f, DateTime.now())
+    }
+
+    /** Find the day a security received the most tweets
+     * @param security A stock symbol or cryptocurrency
+     * @return The most number of tweets about this security in a given day and the date that occurred
+    */ 
+    def historicalMostTweets(security: Security): (Float, DateTime) = {
+        //TODO
+        return (0.0f, DateTime.now())
+    }
+
+    /** Find the day a security received the least tweets
+     * @param security A stock symbol or cryptocurrency
+     * @return The least number of tweets about this security in a given day and the date that occurred
+    */ 
+    def historicalLeastTweets(security: Security): (Float, DateTime) = {
+        //TODO
+        return (0.0f, DateTime.now())
+    }
+}
